d51d0c8d4c3c89b1367727374ea1f1654db8286d
Noah Wood <noahwood@outlook.com>
2021-11-10 17:44:49 -0500

    Adds calculate button styling.

--- lib/constants.dart
+++ lib/constants.dart
@@ -25,6 +25,11 @@ const kLabelTextStyle = TextStyle(
   color: Color(0xFF8D8E98),
 );
 
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
 const kHeavyTextSytle = TextStyle(
   fontSize: 50.0,
   fontWeight: FontWeight.w900,
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -220,9 +220,15 @@ class _InputPageState extends State<InputPage> {
             child: Container(
               color: kBottomBarColor,
               margin: EdgeInsets.only(top: 10.0),
+              padding: EdgeInsets.only(bottom: 20.0),
               width: double.infinity,
               height: kBottomContainerHeight,
-              child: Text('CALCULATE'),
+              child: Center(
+                child: Text(
+                  'CALCULATE',
+                  style: kLargeButtonTextStyle,
+                ),
+              ),
             ),
           )
         ],
